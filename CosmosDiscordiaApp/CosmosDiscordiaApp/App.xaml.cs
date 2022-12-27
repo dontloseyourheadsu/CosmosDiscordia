24b22c5ec372ef1150b5ae2efa21fc600b214dd7
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2022-12-26 19:43:27 -0600

    Created project template

---
+++ CosmosDiscordiaApp/CosmosDiscordiaApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CosmosDiscordiaApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ CosmosDiscordiaApp/CosmosDiscordiaApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CosmosDiscordiaApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
